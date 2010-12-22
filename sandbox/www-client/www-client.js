b70f8a60c1d0cb2851530f4975c719c85841b4bd
Subbu Allamaraju <subbu@subbu.org>
2010-12-22 06:31:05 -0800

    Work in progress

--- sandbox/www-client/www-client.js
+++ sandbox/www-client/www-client.js
@@ -5,7 +5,7 @@ var connect = require('connect'),
   http = require('http'),
   client = require('../../lib/client'),
   querystring = require('querystring'),
-  nstore = require('nstore');
+  nstore = require('nstore')
 
 //
 // This needs to be launched via proxychains
@@ -15,9 +15,9 @@ function manageClients(app) {
   app.resource('/try', {
     // Provide a UI for registering the client
     get: function(req, res) {
-      res.render('try.ejs');
+      res.render('try.ejs')
     }
-  });
+  })
 
   app.resource('/getsomedata', {
 
@@ -30,29 +30,46 @@ function manageClients(app) {
         method: 'GET',
         uri: 'http://localhost:5000/resource',
         proxy: 'http://localhost:3030',
+        headers: {
+          'Authorization' : req.headers.authorization
+        },
         clientError: function(clientRes) {
-          sys.log("ERROR");
+          sys.log("ERROR")
+          res.end()
         },
         success: function(clientRes) {
-          sys.log("SUCCESS");
-          var data = '';
+          sys.log("SUCCESS")
+          var data = ''
           clientRes.on('data', function(chunk) {
-            data += chunk;
-          });
+            data += chunk
+          })
           clientRes.on('end', function() {
-            data = JSON.parse(data);
-            res.render('gotsomedata.ejs', data);
-            res.end();
+            data = JSON.parse(data)
+            res.render('gotsomedata.ejs', data)
+            res.end()
           })
         }
-      });
+      })
     }
 
-  });
+  })
 }
 
-var server = express.createServer();
-server.use(express.bodyDecoder());
-server.use(resource(manageClients));
-server.listen(4000);
-console.log('Client running on port 4000');
+var users = {
+  'user0' : 'password',
+  'user1' : 'password',
+  'user2' : 'password'
+}
+
+
+
+var server = express.createServer()
+
+// todo: use basic auth for now. switch to forms later.
+server.use(connect.basicAuth(function(user, password){
+  return users[user] && users[user].password == password
+}))
+server.use(express.bodyDecoder())
+server.use(resource(manageClients))
+server.listen(4000)
+console.log('Client running on port 4000')
