ec517e0cd8664c41306bbf429083396f13ac059c
Subbu Allamaraju <subbu@subbu.org>
2011-05-10 12:44:03 -0700

    TLS resource server

--- sandbox/resource/resource.js
+++ sandbox/resource/resource.js
@@ -1,39 +1,39 @@
 var connect = require('connect'),
-  resource = require('resource-router'),
-  express = require('express'),
-  crypto = require('crypto'),
-  fs = require('fs'),
-  sys = require('sys');
+    resource = require('resource-router'),
+    express = require('express'),
+    crypto = require('crypto'),
+    fs = require('fs'),
+    sys = require('sys'),
+    https = require('https');
+
+
+var options = {
+  key: fs.readFileSync('keys/s3u-key.pem'),
+  cert: fs.readFileSync('keys/s3u-cert.pem')
+};
 
 function resourceServer(app) {
+    app.resource('/resource', {
+            // Provide a UI for registering the client
+            get: function(req, res) {
+                // TODO: Check the authorization header
 
-  app.resource('/resource', {
-    // Provide a UI for registering the client
-    get: function(req, res) {
-      // TODO: Check the authorization header
-
-      var resource = {
-        name: 'Subbu',
-        text: 'Hi there'
-      };
-      res.writeHead(200, {
-        'Content-Type' : 'application/json',
-        'Connection' : 'close'
-      });
-      res.end(JSON.stringify(resource));
-    }
-  });
+                var resource = {
+                    name: 'Subbu',
+                    text: 'Hi there'
+                };
+                res.writeHead(200, {
+                        'Content-Type' : 'application/json',
+                        'Connection' : 'close'
+                    });
+                res.end(JSON.stringify(resource));
+            }
+        });
 }
 
 
-//var privateKey = fs.readFileSync('privatekey.pem').toString();
-//var certificate = fs.readFileSync('certificate.pem').toString();
-//var credentials = crypto.createCredentials({key: privateKey, cert: certificate});
-
-var server = express.createServer();
-//server.setSecure(credentials);
-server.use(express.bodyDecoder());
+var server = connect.createServer(options);
+server.use(connect.bodyParser());
 server.use(resource(resourceServer));
 server.listen(6000);
 console.log('Resource server running on port 6000');
-
